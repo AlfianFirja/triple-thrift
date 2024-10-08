46d8542e855ac0271954bf6481b4c66d63cfc5ee
AlfianFirja <alfian.firja@gmail.com>
2024-10-08 11:39:39 +0700

    Tugas6

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,16 @@
 from django.forms import ModelForm
 from main.models import ProductEntry
+from django.utils.html import strip_tags
 
 class ProductForm(ModelForm):
     class Meta:
         model = ProductEntry
         fields = ["Product_Name", "Product_Description", "Product_Price"]
+
+    def clean_product_name(self):
+        product_name = self.cleaned_data["product_name"]
+        return strip_tags(product_name)
+
+    def clean_product_description(self):
+        product_description = self.cleaned_data["product_description"]
+        return strip_tags(product_description)
--- main/views.py
+++ main/views.py
@@ -11,16 +11,17 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 @login_required(login_url='/login')
 def show_main(request):
-    product_entries = ProductEntry.objects.filter(user=request.user)
 
     context = {
         'name' : request.user.username,
         'address' : 'Jl. Masjid Al-Farouq (Kos Pondok Ananda)',
         'identitas' : 'Alfian / PBP A / 2306212695',
-        'entries': product_entries,
         'last_login': request.COOKIES['last_login'],
     }
     return render(request, "main.html", context)
@@ -28,9 +29,6 @@ def show_main(request):
 def create_product_entry(request):
     form = ProductForm(request.POST or None)
 
-    # if form.is_valid():
-    #     form.save()
-    #     return redirect('main:show_main')
     if form.is_valid() and request.method == "POST":
         product_entry = form.save(commit=False)
         product_entry.user = request.user
@@ -39,6 +37,28 @@ def create_product_entry(request):
     
     context = {'form': form}
     return render(request, 'create_product_entry.html', context)
+
+@csrf_exempt
+@require_POST
+def add_product_entry_ajax(request):
+    product_name = strip_tags(request.POST.get('product_name'))
+    product_description = strip_tags(request.POST.get('product_description'))
+    product_price = request.POST.get('product_price')
+    user = request.user 
+
+    print(product_name, product_description, product_price, user)
+
+    new_product_entry = ProductEntry(
+        Product_Name=product_name, 
+        Product_Description=product_description, 
+        Product_Price=product_price, 
+        user=user
+    )
+    new_product_entry.save()
+    
+    return HttpResponse(b'CREATED', status=201)
+
+    
 
 def edit_product(request, id):
     # Get mood entry berdasarkan id
@@ -64,11 +84,11 @@ def delete_product(request, id):
     return HttpResponseRedirect(reverse('main:show_main'))
 
 def show_xml(request):
-    data = ProductEntry.objects.all()
+    data = ProductEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = ProductEntry.objects.all()
+    data = ProductEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -93,14 +113,16 @@ def register(request):
 
 def login_user(request):
    if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
+        form = AuthenticationForm(data=request.POST)
 
-      if form.is_valid():
-        user = form.get_user()
-        login(request, user)
-        response = HttpResponseRedirect(reverse("main:show_main"))
-        response.set_cookie('last_login', str(datetime.datetime.now()))
-        return response
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
 
    else:
       form = AuthenticationForm(request)
